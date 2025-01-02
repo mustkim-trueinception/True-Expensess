c0207d1b6269934e59265ccf33bd4c8c214b5a90
mustkim <codesiddhant@gmail.com>
2025-01-02 17:34:12 +0530

    created Project with 3 Screens

---
+++ app/src/main/java/com/example/true_expensess/presentation/screens/screen2.kt
@@ -0,0 +1,13 @@
+package com.example.true_expensess.presentation.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Screen2() {
+
+
+
+    Text(text = "Screen 2")
+}
+
